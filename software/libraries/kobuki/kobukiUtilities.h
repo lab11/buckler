b36d54040853d21b530d322a753842cbebd1013f
Neal Jackson <nealjack@umich.edu>
2020-10-04 17:10:45 -0700

    define uart (un)init

--- software/libraries/kobuki/kobukiUtilities.h
+++ software/libraries/kobuki/kobukiUtilities.h
@@ -7,6 +7,8 @@
 #include "kobukiSensorTypes.h"
 
 int kobukiInit();
+int kobukiUARTInit();
+int kobukiUARTUnInit();
 uint8_t checkSumRead(uint8_t * buffer, int length);
 uint8_t checkSum(uint8_t * buffer, int length);
 
